6d219f3b4de0d7ded2c0d916caf53f04c835bd6e
Achini <achini.nh@gmail.com>
2015-12-13 11:26:33 +0530

    commit inisially

---
+++ src/dataacessobject/DataAcessObject.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dataacessobject;
+
+/**
+ *
+ * @author Achini
+ */
+public class DataAcessObject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        StudentDao studentDao = new StudentDaoImpl();
+
+      //print all students
+      for (Student student : studentDao.getAllStudents()) {
+         System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");
+      }
+
+
+      //update student
+      Student student =studentDao.getAllStudents().get(0);
+      student.setName("Michael");
+      studentDao.updateStudent(student);
+
+      //get the student
+      studentDao.getStudent(0);
+      System.out.println("Student: [RollNo : " + student.getRollNo() + ", Name : " + student.getName() + " ]");		
+   }
+    }
+    
+
+
